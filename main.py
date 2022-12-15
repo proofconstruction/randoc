f5f9c89ca909f382c773ea7490185ee2202fbf81
alex <source@proof.construction>
2022-12-15 11:34:20 -0600

    initial pre-commit hooks run to reformat

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-from randoc import AcademicPaper
-from faker import Faker
 import random
+
+from faker import Faker
 from pdf2image import convert_from_path
+
+from randoc import AcademicPaper
 
 fake = Faker()
 
-title = fake.sentence(nb_words = random.randint(3,7))
+title = fake.sentence(nb_words=random.randint(3, 7))
 authors = []
-for _ in range(random.randint(1,3)):
+for _ in range(random.randint(1, 3)):
     authors.append(fake.name())
 
-doc = AcademicPaper(title, authors, random_text = True)
+doc = AcademicPaper(title, authors, random_text=True)
 doc.fill_document()
 
-doc.generate_pdf('academic_paper', clean_tex=False)
+doc.generate_pdf("academic_paper", clean_tex=False)
 
-filename = 'academic_paper'
+filename = "academic_paper"
 
-pages = convert_from_path(f'{filename}.pdf')
+pages = convert_from_path(f"{filename}.pdf")
 for i, page in enumerate(pages):
-    page.save(f'{filename}-{i}.jpg', 'JPEG')
+    page.save(f"{filename}-{i}.jpg", "JPEG")
--- randoc/academic_paper.py
+++ randoc/academic_paper.py
@@ -1,39 +1,47 @@
-from pylatex import Document, Section, Subsection, Command, NoEscape
-from typing import List
-from faker.providers import lorem
-from faker import Faker
 import random
+from typing import List
+
+from faker import Faker
+from faker.providers import lorem
+from pylatex import Command
+from pylatex import Document
+from pylatex import NoEscape
+from pylatex import Section
+from pylatex import Subsection
+
 
 class AcademicPaper(Document):
-
-    def __init__(self,
-                 title: str = None,
-                 authors: List[str] = None,
-                 date: str = None,
-                 random_text: bool = False):
+    def __init__(
+        self,
+        title: str = None,
+        authors: List[str] = None,
+        date: str = None,
+        random_text: bool = False,
+    ):
         super().__init__()
         self.random_text = random_text
-        self.preamble.append(Command('title', title))
+        self.preamble.append(Command("title", title))
 
         for author in authors:
-            self.preamble.append(Command('author', author))
+            self.preamble.append(Command("author", author))
 
         if date is None:
-            self.preamble.append(Command('date', NoEscape(r'\today')))
+            self.preamble.append(Command("date", NoEscape(r"\today")))
         else:
-            self.preamble.append(Command('date', date))
+            self.preamble.append(Command("date", date))
 
-        self.append(NoEscape(r'\maketitle'))
+        self.append(NoEscape(r"\maketitle"))
 
         self.fake = Faker()
         self.fake.add_provider(lorem)
 
-
-    def fill_document(self,
-                      section_title: str = None,
-                      section_text: str = None,
-                      subsection_title: str = None,
-                      subsection_text: str = None):
+    def fill_document(
+        self,
+        section_title: str = None,
+        section_text: str = None,
+        subsection_title: str = None,
+        subsection_text: str = None,
+    ):
         """Add a section, a subsection and some text to the document."""
 
         if not self.random_text:
@@ -45,13 +53,13 @@ class AcademicPaper(Document):
 
         else:
             with self.create(Section(self.fake.sentence())):
-                num_paragraphs = random.randint(3,5)
+                num_paragraphs = random.randint(3, 5)
                 for _ in range(num_paragraphs):
                     self.append(self.fake.paragraph())
-                    self.append(NoEscape(r'\newline\newline'))
+                    self.append(NoEscape(r"\newline\newline"))
 
                 with self.create(Subsection(self.fake.sentence())):
-                    num_paragraphs = random.randint(3,5)
+                    num_paragraphs = random.randint(3, 5)
                     for _ in range(num_paragraphs):
                         self.append(self.fake.paragraph())
-                        self.append(NoEscape(r'\newline\newline'))
+                        self.append(NoEscape(r"\newline\newline"))
